8f80c94e9ef0cb029db50dfc7da7c23408a392de
jaudick <jaaudick@gmail.com>
2021-04-23 16:08:58 -0400

    fixed infinite jump

--- Game/Assets/Scripts/Player Scripts/BulletEnemyJumpBox.cs
+++ Game/Assets/Scripts/Player Scripts/BulletEnemyJumpBox.cs
@@ -5,6 +5,17 @@ using UnityEngine;
 public class BulletEnemyJumpBox : MonoBehaviour
 {
     public bool canJumpOffEnemy = false;
+    int layerMask = 1 << 8;
+
+    private void Update()
+    {
+        Collider[] enemies = Physics.OverlapBox(transform.position, new Vector3(0.625f*2, 0.218f*3, 0.625f*2), Quaternion.identity, layerMask);
+        if (enemies.Length > 0)
+            canJumpOffEnemy = true;
+        else
+            canJumpOffEnemy = false;
+    }
+    /*
     private void OnTriggerStay(Collider other)
     {
         if (other.GetComponent<JumpOffEnemyBox>() != null && other.gameObject.layer == 8 && (transform.parent.position.y > other.transform.position.y))
@@ -19,5 +30,5 @@ public class BulletEnemyJumpBox : MonoBehaviour
         {
             canJumpOffEnemy = false;
         }
-    }
+    }*/
 }
