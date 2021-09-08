92f43d3e459fb08325ed22a8e811eb05c64aa39c
jaudick <jaaudick@gmail.com>
2021-09-08 16:06:43 -0400

    level 7 updated again and hazard script fixed

--- Game/Assets/Scripts/Hazards/MovingHazard.cs
+++ Game/Assets/Scripts/Hazards/MovingHazard.cs
@@ -167,7 +167,7 @@ public class MovingHazard : MonoBehaviour
                     goingTo1 = true;
                 }
             }
-            //StartCoroutine(LerpPosition());
+            StartCoroutine(LerpPosition());
         }
     }
 
