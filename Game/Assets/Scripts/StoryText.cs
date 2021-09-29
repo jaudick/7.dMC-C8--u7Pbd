a7315fe8b9c930596458a994582b8292e2930c87
jaudick <jaaudick@gmail.com>
2021-09-28 22:12:30 -0400

    tutorial first voicover

--- Game/Assets/Scripts/StoryText.cs
+++ Game/Assets/Scripts/StoryText.cs
@@ -10,6 +10,8 @@ public class StoryText : MonoBehaviour
     public string currentText;
     public int index = 1;
     public int currentTextBox = 0;
+    public float waitFor = 0.075f;
+    public float waitForMultiplier = 6;
     void Start()
     {
         originalText = text[currentTextBox].text;
@@ -30,15 +32,16 @@ public class StoryText : MonoBehaviour
     {
         while(currentText.Length < originalText.Length)
         {
-            float waitFor = 0.075f;
+            float waitFor2 = waitFor;
             //float waitFor = 0.001f;
             text[currentTextBox].text = currentText;
             currentText = originalText.Substring(0, index++);
-            if (index>2 && currentText.ToCharArray()[index-3] == '.')
+            if (index>2 && (currentText.ToCharArray()[index-3] == '.' || currentText.ToCharArray()[index - 3] == ','))
             {
-                waitFor *= 6;
+                waitFor2 *= waitForMultiplier;
             }
-            yield return new WaitForSeconds(waitFor);
+
+            yield return new WaitForSeconds(waitFor2);
         }
         if (text.Length > 1 && ++currentTextBox < text.Length)
             NextTextBox();
