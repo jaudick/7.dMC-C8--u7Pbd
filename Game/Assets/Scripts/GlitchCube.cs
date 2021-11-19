a236b368084c1af6ea4fb3c12e0f5645944c2ae1
jaudick <jaaudick@gmail.com>
2021-11-19 18:49:55 -0500

    glitch cuvbe

--- Game/Assets/Scripts/GlitchCube.cs
+++ Game/Assets/Scripts/GlitchCube.cs
@@ -6,7 +6,6 @@ public class GlitchCube : MonoBehaviour
 {
     public GameObject cube;
     public Vector3 scale;
-    public bool rotationGlitch = true;
 
     void Start()
     {
@@ -17,14 +16,24 @@ public class GlitchCube : MonoBehaviour
     IEnumerator GlitchCubeMethod()
     {
         //cube.SetActive(false);
-        transform.localScale = new Vector3(scale.x * Random.Range(0.25f, 1), scale.y * Random.Range(0.25f, 1), scale.z * Random.Range(0.25f, 1));
-        if (rotationGlitch)
+
+        float time = Random.Range(0.25f, 1f);
+        float timeStarted = Time.time;
+        float percentDone = 0;
+        Vector3 newScale = new Vector3(scale.x * Random.Range(0.25f, 1), scale.y * Random.Range(0.25f, 1), scale.z * Random.Range(0.25f, 1));
+        Vector3 newRotation = new Vector3(Random.Range(-360, 360), Random.Range(-360, 360), Random.Range(-360, 360));
+
+        Vector3 currentScale = transform.localScale;
+        Vector3 currentRotation = transform.eulerAngles;
+
+        while (percentDone<1)
         {
-            transform.eulerAngles = new Vector3(Random.Range(-360, 360), Random.Range(-360, 360), Random.Range(-360, 360));
+            percentDone = (Time.time - timeStarted) * time;
+            transform.localScale = Vector3.Lerp(currentScale, newScale, percentDone);
+            transform.eulerAngles = Vector3.Lerp(currentRotation, newRotation, percentDone);
+            yield return new WaitForEndOfFrame();
         }
-        yield return new WaitForSeconds(Random.Range(0.1f, 0.75f));
-        //cube.SetActive(true);
-        //yield return new WaitForSeconds(Random.Range(0.1f, 0.75f));
+        yield return new WaitForSeconds(Random.Range(0.1f, 1f));
         StartCoroutine(GlitchCubeMethod());
     }
 }
